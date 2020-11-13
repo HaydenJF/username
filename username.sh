9370c13b88f850d048b9a2871b3b05ddea029fd7
Hayden Fargo <fargo@chapman.edu>
2020-11-13 11:16:15 -0800

    Fixes username.sh again again

--- username.sh
+++ username.sh
@@ -5,7 +5,7 @@ echo "Enter a Username: "
 read -r NAME
 while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid username - three to twelve digits only with a starting letter and with only letters numbers and underscores!"
+	echo "You must enter a valid username - three to twelve digits only with a starting lower case letter and with only lower case letters, numbers and underscores!"
 	echo "Enter a Username: "
 	read -r NAME
 done
