0a97cb16521749c784484b98b9256c6c7fada4e1
Hayden Fargo <fargo@chapman.edu>
2020-11-13 11:11:20 -0800

    Fixes username.sh

--- username.sh
+++ username.sh
@@ -3,10 +3,10 @@
 # author Hayden Fargo
 echo "Enter a Username: "
 read -r NAME
-while echo "$NAME" | grep -E -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][0-9a-z_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must enter a valid username - three to twelve digits only with a starting letter and with only letters numbers and underscores!"
+	echo "Enter a username code: "
 	read -r NAME
 done
 echo "Thank you"
